b73f0758c77a0759a58014c72aaf62a86e30f76b
Alicia Yang <74331667+Alicia-y@users.noreply.github.com>
2022-02-05 22:42:13 -0500

    discussion

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,14 +1,12 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Clean the survey data downloaded from Toronto Open Data Portal
+# Author: Alicia Yang
+# Data: 6 February 2021
+# Contact: aliciam.yang@utoronto.ca
 # License: MIT
 # Pre-requisites: 
 # - Need to have downloaded the ACS data and saved it to inputs/data
 # - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
 
 
 #### Workspace setup ####
@@ -22,24 +20,13 @@ library(dplyr)
 # get package
 package <- show_package("0b6d3a00-7de1-440b-b47c-7252fd13929f")
 
-package
-
-# get all resources for this package
-resources <- list_package_resources("0b6d3a00-7de1-440b-b47c-7252fd13929f")
-
-# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
-datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
-
 data1 <-
   list_package_resources("0b6d3a00-7de1-440b-b47c-7252fd13929f") %>%
   get_resource()
 
 # get package
 package2 <- show_package("9fcff3e1-3737-43cf-b410-05acd615e27b")
-package2
 
-# get all resources for this package
-resources2 <- list_package_resources("9fcff3e1-3737-43cf-b410-05acd615e27b")
 
 data2 <-
   list_package_resources("9fcff3e1-3737-43cf-b410-05acd615e27b") %>%
